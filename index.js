b52dc34c04f45a8fbd046becf53480b80466f85b
Bhuvaneswaran0 <bhuvaneshmk060@gmail.com>
2026-07-08 10:04:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// Temperature Converter using map()
++// Temperature Converter using map()
 
 const celsius = [0, 20, 30, 40];
+ const arr1=[1,2,3,4,5];
+const arr2=arr1.map(C × 9/5);
+console.log(arr2)
 
 // TODO:
 // Convert the Celsius temperatures to Fahrenheit
